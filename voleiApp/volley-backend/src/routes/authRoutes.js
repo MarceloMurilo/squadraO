17f10cea13c05167782ed37ac26766462affa8b1
Marcelo Murilo <108279750+MarceloMurilo@users.noreply.github.com>
2024-10-28 12:21:55 -0300

    add docker

--- voleiApp/volley-backend/src/routes/authRoutes.js
+++ voleiApp/volley-backend/src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const pool = require('../db');
 const router = express.Router();
 
