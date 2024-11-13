1a1f2a87e6380a1083b627f4995fdc6dc9950fe8
Carlos Daniel <86565575+CDanielBF@users.noreply.github.com>
2024-11-13 14:54:40 -0300

    Bolinhadegude (#9)
    * commit de telas de login, steps e register

--- voleiApp/volley-frontend/src/screens/RegisterScreen.js
+++ voleiApp/volley-frontend/src/screens/RegisterScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import { register } from '../services/authService';
 
 const RegisterScreen = ({ navigation }) => {
@@ -24,45 +24,159 @@ const RegisterScreen = ({ navigation }) => {
     }
   };
 
-  // Aqui você coloca o conteúdo correto a ser renderizado
   return (
-    <View style={styles.container}>
-      <Text>Nome:</Text>
-      <TextInput
-        style={styles.input}
-        value={name}
-        onChangeText={setName}
-        placeholder="Digite seu nome"
-      />
-      <Text>Email:</Text>
-      <TextInput
-        style={styles.input}
-        value={email}
-        onChangeText={setEmail}
-        placeholder="Digite seu email"
-      />
-      <Text>Password:</Text>
-      <TextInput
-        style={styles.input}
-        value={password}
-        onChangeText={setPassword}
-        placeholder="Digite sua senha"
-        secureTextEntry
-      />
-      <Button title="Registrar" onPress={handleRegister} />
+    <View style={styles.Body}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Bom te conhecer!</Text>
+      </View>
+
+      <View style={styles.Main}>
+        <View style={styles.container}>
+          <View style={styles.inputContainer}>
+            <View style={styles.bgTextNome}>
+              <Text style={styles.inputName}>Nome Completo</Text>
+            </View>
+            <TextInput
+              style={styles.input}
+              value={name}
+              onChangeText={setName}
+              placeholder="Digite seu nome"
+            />
+          </View>
+
+          <View style={styles.inputContainer}>
+            <View style={styles.bgTextEmail}>
+              <Text style={styles.inputName}>Email</Text>
+            </View>
+            <TextInput
+              style={styles.input}
+              value={email}
+              onChangeText={setEmail}
+              placeholder="Digite seu email"
+            />
+          </View>
+
+          <View style={styles.inputContainer}>
+            <View style={styles.bgTextPassword}>
+              <Text style={styles.inputName}>Senha</Text>
+            </View>
+            <TextInput
+              style={styles.input}
+              value={password}
+              onChangeText={setPassword}
+              placeholder="Digite sua senha"
+              secureTextEntry
+            />
+          </View>
+
+          <TouchableOpacity style={styles.butReg} onPress={handleRegister}>
+            <Text style={styles.buttonText}>Cadastrar</Text> 
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  header: {
+    width: '100%',
+    height: 100,
+    alignItems: 'center',
+  },
+  Body: {
+    backgroundColor: '#37A0EC',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  Main: {
+    backgroundColor: '#ffffff',
+    height: 622,
+    borderRadius: 39,
+    top: 25,
+    width: '100%',
+  },
+  title: {
+    color: 'white',
+    fontSize: 30,
+    position: 'relative',
+    top: 70,
+  },
   container: {
     padding: 20,
+    paddingTop: 40,
+  },
+  inputContainer: {
+    marginTop: 5,
+    position: 'relative',
+    alignItems: 'center',
   },
   input: {
-    borderBottomWidth: 1,
+    borderStyle: 'solid',
+    borderRadius: 10,
+    borderWidth: 1,
     marginBottom: 20,
-    padding: 8,
+    borderColor: '#FF7014',
+    padding: 10,
+    width: 290,
+    height: 50,
   },
+  bgTextNome: {
+    alignItems: 'center',
+    borderRadius: 50,
+    width: 130,
+    height: 25,
+    backgroundColor: 'white',
+    position: 'absolute',
+    top: -13,
+    left: 40,
+    paddingBottom: 5,
+    zIndex: 1,  
+  },
+  bgTextEmail: {
+    alignItems: 'center',
+    borderRadius: 50,
+    width: 55,
+    height: 25,
+    backgroundColor: 'white',
+    position: 'absolute',
+    top: -13,
+    left: 40,
+    paddingBottom: 5,
+    zIndex: 1,  
+  },
+  bgTextPassword: {
+    alignItems: 'center',
+    borderRadius: 50,
+    width: 59,
+    height: 25,
+    backgroundColor: 'white',
+    position: 'absolute',
+    top: -13,
+    left: 40,
+    paddingBottom: 5,
+    zIndex: 1,  
+  },
+  inputName: {
+    fontSize: 14,
+    color: '#37A0EC',
+  },
+  butReg: {
+    background: 'none', 
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: '#FF7014',
+    height: 45,
+    width: 120,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    left: 197,
+  },
+  buttonText: {
+    color: '#FF7014',
+  }
 });
 
 export default RegisterScreen;
